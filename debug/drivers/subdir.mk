6e0a660fbb6b6c509c7e07ae65225e3d51c47146
Greg Pohodich <greg@pohodich.com>
2017-03-15 08:38:20 -0400

    Initial Project File Commit
    Initial Project File Commit

---
+++ debug/drivers/subdir.mk
@@ -0,0 +1,48 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/fsl_clock.c \
+../drivers/fsl_common.c \
+../drivers/fsl_dmamux.c \
+../drivers/fsl_edma.c \
+../drivers/fsl_flash.c \
+../drivers/fsl_ftm.c \
+../drivers/fsl_gpio.c \
+../drivers/fsl_smc.c \
+../drivers/fsl_uart.c 
+
+OBJS += \
+./drivers/fsl_clock.o \
+./drivers/fsl_common.o \
+./drivers/fsl_dmamux.o \
+./drivers/fsl_edma.o \
+./drivers/fsl_flash.o \
+./drivers/fsl_ftm.o \
+./drivers/fsl_gpio.o \
+./drivers/fsl_smc.o \
+./drivers/fsl_uart.o 
+
+C_DEPS += \
+./drivers/fsl_clock.d \
+./drivers/fsl_common.d \
+./drivers/fsl_dmamux.d \
+./drivers/fsl_edma.d \
+./drivers/fsl_flash.d \
+./drivers/fsl_ftm.d \
+./drivers/fsl_gpio.d \
+./drivers/fsl_smc.d \
+./drivers/fsl_uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/%.o: ../drivers/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -ffreestanding -fno-builtin -Wall  -g -DDEBUG -DCPU_MK64FN1M0VMD12 -DFRDM_K64F -DFREEDOM -I../CMSIS/Include -I../devices/gcc -I../devices -I../drivers -I../sources -I../utilities -std=gnu99 -mapcs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ debug/sources/ftm_simple_pwm.d
@@ -0,0 +1,55 @@
+sources/ftm_simple_pwm.o: ../sources/ftm_simple_pwm.c \
+ ../utilities/fsl_debug_console.h ../drivers/fsl_common.h \
+ ../devices/fsl_device_registers.h ../devices/MK64F12.h \
+ ../CMSIS/Include/core_cm4.h ../CMSIS/Include/core_cmInstr.h \
+ ../CMSIS/Include/cmsis_gcc.h ../CMSIS/Include/core_cmFunc.h \
+ ../CMSIS/Include/core_cmSimd.h ../devices/system_MK64F12.h \
+ ../devices/MK64F12_features.h ../drivers/fsl_clock.h \
+ ../drivers/fsl_common.h ../sources/board.h ../sources/clock_config.h \
+ ../drivers/fsl_gpio.h ../drivers/fsl_ftm.h ../drivers/fsl_edma.h \
+ ../drivers/fsl_dmamux.h ../drivers/fsl_port.h ../sources/Color.h \
+ ../sources/pin_mux.h
+
+../utilities/fsl_debug_console.h:
+
+../drivers/fsl_common.h:
+
+../devices/fsl_device_registers.h:
+
+../devices/MK64F12.h:
+
+../CMSIS/Include/core_cm4.h:
+
+../CMSIS/Include/core_cmInstr.h:
+
+../CMSIS/Include/cmsis_gcc.h:
+
+../CMSIS/Include/core_cmFunc.h:
+
+../CMSIS/Include/core_cmSimd.h:
+
+../devices/system_MK64F12.h:
+
+../devices/MK64F12_features.h:
+
+../drivers/fsl_clock.h:
+
+../drivers/fsl_common.h:
+
+../sources/board.h:
+
+../sources/clock_config.h:
+
+../drivers/fsl_gpio.h:
+
+../drivers/fsl_ftm.h:
+
+../drivers/fsl_edma.h:
+
+../drivers/fsl_dmamux.h:
+
+../drivers/fsl_port.h:
+
+../sources/Color.h:
+
+../sources/pin_mux.h:
